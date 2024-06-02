79afe6c7837cf81e9b498a47dfeb9b775d5199a8
anronella <ant.valdesr@duocuc.cl>
2024-06-01 20:43:16 -0400

    api zapatillas terminada

---
+++ zapatillas.js
@@ -0,0 +1,76 @@
+const  zapatillas = [
+    {
+        "id":1,
+        "nombre": "DC CURE HIGH-TOP", 
+        "imagen": "https://www.kliper.cl/media/catalog/product/a/d/adys400072_dcwyr_1_1.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=265&width=265&canvas=265:265",
+        "marca": "dc shoes",
+        "precio": "$64.990",
+        "tipo": "hombre"
+        
+    }
+    ,
+    {
+        "id":2,
+        "nombre": " Dc Manteca 4 HI Hombre Negro ", 
+        "imagen": "https://i.pinimg.com/736x/c2/38/0d/c2380d05c2e4eb714541a255fc4d09d3.jpg",
+        "marca": "dc shoes",
+        "precio": "$74.990",
+        "tipo": "hombre"
+        
+    }
+    ,
+    {
+        "id":3,
+        "nombre": "Zapatillas New Balance 530", 
+        "imagen": "https://http2.mlstatic.com/D_NQ_NP_923720-MLC74842325828_032024-O.webp",
+        "marca": "New Balance",
+        "precio": "$79.990",
+        "tipo": "hombre"
+        
+    }
+    ,
+    {
+        "id":4,
+        "nombre": "Vulcd Zapatilla Urbana Hombre Negro Fila 40", 
+        "imagen": "https://falabella.scene7.com/is/image/Falabella/50353932_1",
+        "marca": "Fila",
+        "precio": "$59.900",
+        "tipo": "hombre"
+        
+    }
+    ,
+    {
+        "id":5,
+        "nombre": "Zapatillas New Balance Unisex 530 Blanco/Beige", 
+        "imagen": "https://api-prd.ynk.cl/medias/515Wx515H-ANTIHUMAN-NBMR530RD-VIEW3.jpg?context=bWFzdGVyfGltYWdlc3wxNDQ2MnxpbWFnZS9qcGVnfGFHTXhMMmczTnk4NU5UazNPVE0zTkRnek9EQTJMelV4TlZkNE5URTFTRjlCVGxSSlNGVk5RVTVmVGtKTlVqVXpNRkpFWDFaSlJWY3pMbXB3Wnd8MDdiYjEzMzJmMDQwMjJhYWMwNWZhZDBlMTg2ZTI3MzQxMDBlNzczMTEzY2UzNzA1N2NlYTBlNTU1OTBjOGQxNw",
+        "marca": "New Balance",
+        "precio": "$79.990",
+        "tipo": "hombre"
+        
+    }
+    ,
+    {
+        "id":6,
+        "nombre": "Suede Xl Zapatilla Urbana Hombre Rojo Puma", 
+        "imagen": "https://paralelstore.com/28134-superlarge_default/zapatillas-converse-cons-fastbreak-pro-white-vaporous-gray.jpg",
+        "marca": "Puma",
+        "precio": "$79.990",
+        "tipo": "hombre"
+        
+    }
+    ,
+    {
+        "id":7,
+        "nombre": "Puelo Cs Hi Acero Zapatilla Outdoor Hombre Impermeable Gris Lippi", 
+        "imagen": "https://static.dafiti.cl/p/lippi-6420-6005701-1-zoom.jpg",
+        "marca": "Lippi",
+        "precio": "$84.990",
+        "tipo": "hombre"
+        
+    }
+    
+    
+]
+module.exports = {
+    zapatillas
+}
