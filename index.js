69130115efa4de2698fc21536cecedde77921aee
antonella <ant.valdesr@duocuc.cl>
2024-06-04 18:59:51 -0400

    api terminada2

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+const {getzapatillas , getzapatillasById} = require("./contollers/eqiposcontroller")
 
+app.get("/", getzapatillas);
+
+app.get("/:id", getzapatillasById);
 
 app.listen(port, ()=>{
     console.log (`conectado correctamente al puerto: ${port}`)
